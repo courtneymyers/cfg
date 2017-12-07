2ee0eadbdb15c56aa93dae3d974090f9cbf4069c
Courtney Myers <courtney.aaron.myers@gmail.com>
2017-12-07 11:06:53 -0500

    re-organize aliases

--- .cfg/bash/aliases.bash
+++ .cfg/bash/aliases.bash
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# better directory listing
-alias ll='ls -lahG'
-
-# colorize grep
-alias grep='grep --color=auto'
-
 # navigating directories
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -14,6 +8,12 @@ alias ....='cd ../../..'
 # common directories
 alias sites='cd ~/Sites/'
 alias projects='cd ~/Projects/'
+
+# better directory listing
+alias ll='ls -lahG'
+
+# colorize grep
+alias grep='grep --color=auto'
 
 # show preview window with fzf
 alias fzf='fzf --border --reverse --height 50% --preview "head -100 {}"'
