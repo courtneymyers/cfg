8136ab8634c0b702bb3804b0cfea3ff9c1897d64
Courtney Myers <courtney.aaron.myers@gmail.com>
2017-12-17 00:12:23 -0500

    Enable autocompletion from various files in /usr/local/etc/bach_completion.d directory

--- .cfg/bash/autocomplete.bash
+++ .cfg/bash/autocomplete.bash
@@ -11,3 +11,8 @@ _pip_completion()
                    PIP_AUTO_COMPLETE=1 $1 ) )
 }
 complete -o default -F _pip_completion pip
+
+# enable autocompletion from files in bash_completion.d directory
+for file in /usr/local/etc/bash_completion.d/* ; do
+  source $file
+done
