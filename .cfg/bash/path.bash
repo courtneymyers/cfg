7c5c9e2cc7d6e18b98f3c120655df4d64097b877
Courtney Myers <courtney.aaron.myers@gmail.com>
2017-12-30 21:30:47 -0500

    Update comment about ~/.local/bin usage

--- .cfg/bash/path.bash
+++ .cfg/bash/path.bash
@@ -3,5 +3,6 @@
 # add composer’s globally installed packages to path
 export PATH=$HOME/.composer/vendor/bin:$PATH
 
-# add directory haskell stack uses to path
+# add ~/.local/bin directory to path
+# (used by `pip install --user` packages and haskell stack)
 export PATH=$HOME/.local/bin:$PATH
