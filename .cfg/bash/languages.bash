b52b7491f9382559e84ee7fbe6f6d2e7bc7bc585
Courtney Myers <courtney.aaron.myers@gmail.com>
2018-11-11 14:33:03 -0500

    Remove obsolete php-version from setup and languages scripts

--- .cfg/bash/languages.bash
+++ .cfg/bash/languages.bash
@@ -9,9 +9,6 @@ eval "$(pyenv init -)"
 # enable shims and autocompletion for jenv
 eval "$(jenv init -)"
 
-# enable php-version
-source $(brew --prefix php-version)/php-version.sh && php-version 7
-
 # enable n (node)
 export N_PREFIX="$HOME/Utilities/n";
 [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
